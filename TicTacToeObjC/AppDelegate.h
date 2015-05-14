53600cda06f5d008edb287bbf89dea6b7aad276d
lettemax <maxlettenberger@Maxs-MacBook-Pro.local>
2015-05-14 14:20:32 -0700

    Initial Commit

---
+++ TicTacToeObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToeObjC
+//
+//  Created by Max Lettenberger on 5/14/15.
+//  Copyright (c) 2015 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
